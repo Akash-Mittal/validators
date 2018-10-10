1f7634b9c26fdedcdbbf9c9c58a47b4f7f532f66
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-10-10 12:07:49 +0530

    Changes:1.) Increemental.

--- src/main/java/com/am/innovations/Validators.java
+++ src/main/java/com/am/innovations/Validators.java
@@ -3,7 +3,8 @@ package com.am.innovations;
 import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
-public interface Validators<T> extends Predicate<T> {
+public interface Validators extends Predicate<Number> {
+
 	BiPredicate<Number, Number> checkIfEqualsTo = (input, constraint) -> input.intValue() == constraint.intValue();
 	BiPredicate<Number, Number> checkIfGreaterThan = (input, constraint) -> input.intValue() == constraint.intValue();
 	BiPredicate<Number, Number> checkIfLessThan = (input, constraint) -> input.intValue() == constraint.intValue();
