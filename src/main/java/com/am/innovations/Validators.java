984b88949b4a418e03ae7a5d22c3155660a0dc6f
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-10-10 12:46:42 +0530

    Changes:1.) Fixed Bug.

--- src/main/java/com/am/innovations/Validators.java
+++ src/main/java/com/am/innovations/Validators.java
@@ -6,6 +6,6 @@ import java.util.function.Predicate;
 public interface Validators extends Predicate<Number> {
 
 	BiPredicate<Number, Number> checkIfEqualsTo = (input, constraint) -> input.intValue() == constraint.intValue();
-	BiPredicate<Number, Number> checkIfGreaterThan = (input, constraint) -> input.intValue() == constraint.intValue();
-	BiPredicate<Number, Number> checkIfLessThan = (input, constraint) -> input.intValue() == constraint.intValue();
+	BiPredicate<Number, Number> checkIfGreaterThan = (input, constraint) -> input.intValue() > constraint.intValue();
+	BiPredicate<Number, Number> checkIfLessThan = (input, constraint) -> input.intValue() < constraint.intValue();
 }
