580b8d801e9858997a2b93ba99f29c9337733671
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-10-10 15:22:30 +0530

    Changes:1.) Array Based.

--- src/main/java/com/am/innovations/Validators.java
+++ src/main/java/com/am/innovations/Validators.java
@@ -5,6 +5,11 @@ import java.util.function.Predicate;
 
 public interface Validators extends Predicate<Number> {
 
+	Predicate<Object> checkIfNotNull = input -> input != null;
+
+	BiPredicate<Object[], Integer> checkIfNotNullAndSizeGreaterThan = (input, size) -> checkIfNotNull.test(input)
+			&& Validators.checkIfGreaterThan.test(input.length, size);
+
 	BiPredicate<Number, Number> checkIfEqualsTo = (input, constraint) -> input.intValue() == constraint.intValue();
 	BiPredicate<Number, Number> checkIfGreaterThan = (input, constraint) -> input.intValue() > constraint.intValue();
 	BiPredicate<Number, Number> checkIfLessThan = (input, constraint) -> input.intValue() < constraint.intValue();
