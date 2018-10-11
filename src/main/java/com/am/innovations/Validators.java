76567a2f4e4c55a782095b7ae6ca68f3b59565d6
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-10-11 10:28:32 +0530

    Changes:1.) Increemental

--- src/main/java/com/am/innovations/Validators.java
+++ src/main/java/com/am/innovations/Validators.java
@@ -13,4 +13,24 @@ public interface Validators extends Predicate<Number> {
 	BiPredicate<Number, Number> checkIfEqualsTo = (input, constraint) -> input.intValue() == constraint.intValue();
 	BiPredicate<Number, Number> checkIfGreaterThan = (input, constraint) -> input.intValue() > constraint.intValue();
 	BiPredicate<Number, Number> checkIfLessThan = (input, constraint) -> input.intValue() < constraint.intValue();
+
+	enum LIMITS {
+		ZERO(0), ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10);
+
+		private LIMITS(int limit) {
+			this.limit = limit;
+		}
+
+		private int limit;
+
+		public int getLimit() {
+			return limit;
+		}
+
+		public void setLimit(int limit) {
+			this.limit = limit;
+		}
+
+	}
+
 }
